3170a029c97a6423e9de1d94564058f699533469
Arian Pieroni <rizelloyd@gmail.com>
2026-01-16 09:13:05 +0100

    Added background

--- core/src/main/java/org/lasarimanstudios/escapedungeon/Main.java
+++ core/src/main/java/org/lasarimanstudios/escapedungeon/Main.java
@@ -1,11 +1,68 @@
 package org.lasarimanstudios.escapedungeon;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends Game {
-    @Override
-    public void create() {
-        setScreen(new FirstScreen());
-    }
+public class Main implements ApplicationListener {
+	Texture backgroundTexture;
+	SpriteBatch spriteBatch;
+	FitViewport viewport;
+
+	@Override
+	public void create() {
+		backgroundTexture = new Texture("test.png");
+		spriteBatch = new SpriteBatch();
+		viewport = new FitViewport(8, 5);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+	}
+
+	@Override
+	public void render() {
+		input();
+		logic();
+		draw();
+	}
+
+	private void input() {
+	}
+
+	private void logic() {
+	}
+
+	private void draw() {
+		ScreenUtils.clear(Color.BLACK);
+		viewport.apply();
+		spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+		spriteBatch.begin();
+
+		float worldWidth = viewport.getWorldWidth();
+		float worldHeight = viewport.getWorldHeight();
+
+		spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+
+		spriteBatch.end();
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
 }
